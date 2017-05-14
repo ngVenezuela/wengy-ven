8846f37b17073d5dea597b1f17feb2af8bbdcef9
Leonardo Cabeza <cabeza.leo@gmail.com>
2017-05-14 17:38:44 -0500

    (Agrega) Se hizo la integración del bot con API.AI
    Close #63

--- config/config.sample.js
+++ config/config.sample.js
@@ -1,8 +1,18 @@
+const uuidV4 = require('uuid/v4');
+
 const config = {
-  telegramToken: 'MY_SUPER_SECRET_TOKEN',
+  telegramToken: 'MY_SUPER_SECRET_TELEGRAM_TOKEN',
   morningHour: 7,
   groupId: -1001031605415,
   // groupId: -165387746 //test group,
+  botUsername: 'WengyBot',
+  // botUsername: 'LeoTestsBot', // test bot username
+  apiAI: {
+    lang: 'es',
+    sessionId: uuidV4(),
+    queryUrl: 'https://api.api.ai/v1/query?v=20150910',
+    clientAccessToken: 'MY_SUPER_SECRET_APIAI_TOKEN'
+  },
   goodMorningRegExp: new RegExp('buen(os)*\\sd[ií]+as', 'iu'),
   blogFeedUrl: 'https://medium.com/feed/ngvenezuela',
 };
