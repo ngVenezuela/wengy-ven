1610c2888ede244f61a13ee97754178037145af3
Alfonso Andrés López Molina <osnoser_12@hotmail.com>
2017-05-25 19:32:00 -0400

    (Modifica): Envío de múltiples mensajes por item en releases de GitHub.

--- src/utils/github-release.js
+++ src/utils/github-release.js
@@ -10,23 +10,25 @@ const sendRelease = (bot, release, repository, changelogExist) => {
     return;
   }
 
-  const last = release.items[0];
   const name = repository.match(/[\w.-]+$/gi)[0];
-  const tag = last.id.match(/[\w.-]+$/gi)[0];
 
-  sendMessage(
-    bot,
-    config.groupId,
-    githubReleaseMessage
-      .replace('#{name}', name)
-      .replace('#{version}', tag)
-      .replace(
+  release.items.forEach(item => {
+    const tag = item.id.match(/[\w.-]+$/gi)[0];
+
+    sendMessage(
+      bot,
+      config.groupId,
+      githubReleaseMessage
+        .replace('#{name}', name)
+        .replace('#{version}', tag)
+        .replace(
         '#{url}',
         changelogExist
           ? `https://github.com/${repository}/blob/master/CHANGELOG.md`
           : `https://github.com/${repository}/releases/tag/${tag}`
-      )
-  );
+        )
+    );
+  });
 };
 
 module.exports = {
