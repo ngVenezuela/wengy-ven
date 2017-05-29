7a12bd0c40419b79618da9df65d7f35d13aa636b
Leonardo Cabeza <leocabeza@users.noreply.github.com>
2017-05-28 19:24:07 -0500

    (Agrega) Control de comandos mediante redis (#99)

--- src/utils/chat.js
+++ src/utils/chat.js
@@ -4,75 +4,77 @@ const messages = require('./../../config/messages');
 const config = require('./../../config/config.js');
 const sendMessage = require('./../utils/send-message');
 const generateRandom = require('./../utils/time').generateRandom;
+const commandUtility = require('./../utils/command');
 
-function checkGoodMorning(goodMorningGivenToday, text) {
-  return !goodMorningGivenToday && config.goodMorningRegExp.test(text);
-}
+const checkGoodMorning = (goodMorningGivenToday, text) =>
+  !goodMorningGivenToday && config.goodMorningRegExp.test(text);
 
-function checkForCode(bot, msg) {
-  if (!Object.prototype.hasOwnProperty.call(msg, 'entities')) {
-    return;
-  }
+const checkForCode = (bot, msgContext, redisClient) => {
+  commandUtility.verifyCommand(redisClient, '/gist', msgContext.from.id)
+    .then((canExecuteCommand) => {
+      if (canExecuteCommand) {
+        if (!Object.prototype.hasOwnProperty.call(msgContext, 'entities')) {
+          return;
+        }
 
-  if (msg.entities[0].type !== 'pre') {
-    return;
-  }
+        if (msgContext.entities[0].type !== 'pre') {
+          return;
+        }
 
-  if (msg.text.length >= 200) {
-    return;
-  }
+        if (msgContext.text.length >= 200) {
+          return;
+        }
 
-  const chatId = msg.chat.id;
-  const { firstName = '', lastName = '', username = '' } = msg.from;
-  const fullName = firstName === '' && lastName === '' ? '' : `${firstName} ${lastName} `;
-  const user = username === '' ? '' : `(@${username})`;
-  const filename = `${new Date().toISOString()}.js`;
-  const gist = msg.text;
+        const chatId = msgContext.chat.id;
+        const { firstName = '', lastName = '', username = '' } = msgContext.from;
+        const fullName = firstName === '' && lastName === '' ? '' : `${firstName} ${lastName} `;
+        const user = username === '' ? '' : `(@${username})`;
+        const filename = `${new Date().toISOString()}.js`;
+        const gist = msgContext.text;
 
-  const body = {
-    description: 'gist creado por '.concat(fullName).concat(user).concat(
-      ' para https://t.me/ngvenezuela con https://github.com/ngVenezuela/wengy-ven'),
-    public: true,
-    files: {
-      [filename]: {
-        content: gist
+        const body = {
+          description: 'gist creado por '.concat(fullName).concat(user).concat(
+            ' para https://t.me/ngvenezuela con https://github.com/ngVenezuela/wengy-ven'),
+          public: true,
+          files: {
+            [filename]: {
+              content: gist
+            }
+          }
+        };
+
+        fetch('https://api.github.com/gists', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+          },
+          body: JSON.stringify(body)
+        })
+          .then(response => response.json())
+          .then(({ html_url }) => {
+            sendMessage(bot, chatId, html_url, true, msgContext.message_id);
+          }).catch(() => { });
       }
-    }
-  };
+    })
+    .catch(() => { });
+};
 
-  fetch('https://api.github.com/gists', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8',
-    },
-    body: JSON.stringify(body)
-  })
-  .then(response => response.json())
-  .then(({ html_url }) => {
-    sendMessage(bot, chatId, html_url, true, msg.message_id);
-  }).catch(() => {});
-}
+const formatName = (firstName, userName) => (userName ? '@'.concat(userName) : firstName);
 
-function formatName(msgContext) {
-  return msgContext.username
-    ? '@'.concat(msgContext.username)
-    : msgContext.first_name;
-}
-
-function sayHello(bot, msg) {
+const sayHello = (bot, msg) => {
   sendMessage(
     bot,
     msg.chat.id,
     messages.welcome.replace(
       '#{name}',
-      formatName(msg.new_chat_member)
+      formatName(msg.new_chat_member.first_name, msg.new_chat_member.username)
     ),
     true,
     msg.message_id
   );
-}
+};
 
-function sayGoodbye(bot, msg) {
+const sayGoodbye = (bot, msg) => {
   const randomIndex = generateRandom(0, messages.goodByes.length - 1);
 
   sendMessage(
@@ -80,12 +82,12 @@ function sayGoodbye(bot, msg) {
     msg.chat.id,
     messages.goodByes[randomIndex].replace(
       '#{name}',
-      formatName(msg.left_chat_member)
+      formatName(msg.left_chat_member.first_name, msg.left_chat_member.username)
     ),
     true,
     msg.message_id
   );
-}
+};
 
 module.exports = {
   sayHello,
---
+++ src/utils/command.js
@@ -0,0 +1,43 @@
+const MAX_COMMANDS_PER_MINUTE_AND_USER = 5;
+
+const verifyCommand = async (redisClient, command, userId) => {
+  try {
+    const commandUserValue = await redisClient.getAsync(`${userId}-${command}`);
+
+    // if user already has max commands per minute, return false
+    if (parseInt(commandUserValue, 10) === MAX_COMMANDS_PER_MINUTE_AND_USER) {
+      return false;
+    }
+
+    /*
+      * else, check existing ttl, and set key again
+      * for future reference, redisClient.incr will do what we want,
+      * but, if key already expired, it will create one with no expiration time
+    */
+    const ttlRemaining = await redisClient.ttlAsync(`${userId}-${command}`);
+
+    if (ttlRemaining > 0) {
+      const newSet = await redisClient.setAsync(
+        `${userId}-${command}`,
+        parseInt(commandUserValue, 10) + 1,
+        'EX',
+        ttlRemaining
+      );
+      return newSet === 'OK';
+    }
+
+    /*
+      * create key again
+      * ttlReply might be -2, it means key doesn't exist
+      * and it could happen if ttl is close to zero
+    */
+    const newSet = await redisClient.setAsync(`${userId}-${command}`, 1, 'EX', 60);
+    return newSet === 'OK';
+  } catch (error) {
+    return false;
+  }
+};
+
+module.exports = {
+  verifyCommand
+};
