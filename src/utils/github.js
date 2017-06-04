5735923fee64cf4bb7fa3a5754b7270a8143e4d6
Leonardo Cabeza <cabeza.leo@gmail.com>
2017-06-04 18:24:16 -0500

    (Agrega): Comando para crear gists
    También recomienda al usuario usar el comando cuando coloca un código mayor a 200 caracteres

--- src/utils/github.js
+++ src/utils/github.js
@@ -7,12 +7,14 @@ const githubOpenVeLinkMessage = require('./../../config/messages').githubOpenVeL
 const gistCreated = require('./../../config/messages').gistCreated;
 const telegramLink = require('./../../config/config').community.telegram.link;
 const githubLink = require('./../../config/config').community.github;
+const gistRecommendation = require('./../../config/messages').gistRecommendation;
 
 const sendMessage = require('./../utils/send-message');
 const commandUtility = require('./../utils/command');
 
 const GITHUB_LINK_OPENVE_TELEGRAM_COMMUNITY = 'https://github.com/OpenVE/comunidades-en-telegram';
 const GIST_COMMAND = '/gist';
+const MAX_LENGTH_GIST_TEXT = 200;
 
 const isItAGithubRelease = (repository, feed) =>
   feed && feed.status.feed && feed.status.feed.search(repository) !== -1;
@@ -22,10 +24,10 @@ const sendRelease = (bot, release, repository, changelogExist) => {
     return;
   }
 
-  const name = repository.match(/[\w.-]+$/gi)[0];
+  const name = repository.match(/[\w\.-]+$/gi)[0]; // eslint-disable-line no-useless-escape
 
   release.items.forEach((item) => {
-    const tag = item.id.match(/[\w.-]+$/gi)[0];
+    const tag = item.id.match(/[\w\.-]+$/gi)[0]; // eslint-disable-line no-useless-escape
 
     sendMessage(
       bot,
@@ -52,57 +54,67 @@ const checkAndSendRelease = (bot, feed) => {
   }
 };
 
-const checkForCode = (bot, msgContext, redisClient) => {
+const prepareAndSendGist = (bot, msgContext, checkingForCode, code = '') => {
+  const chatId = msgContext.chat.id;
+  const { firstName = '', lastName = '', username = '' } = msgContext.from;
+  const fullName = firstName === '' && lastName === '' ? '' : `${firstName} ${lastName} `;
+  const user = username === '' ? '' : `(@${username})`;
+  const filename = `${new Date().toISOString()}.js`;
+  const gist = checkingForCode ? msgContext.text : code;
+
+  const body = {
+    description: gistCreated
+      .replace('#{fullName}', fullName)
+      .replace('#{user}', user)
+      .replace('#{telegramLink}', telegramLink)
+      .replace('#{githubLink}', githubLink),
+    public: true,
+    files: {
+      [filename]: {
+        content: gist
+      }
+    }
+  };
+
+  fetch('https://api.github.com/gists', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+    body: JSON.stringify(body)
+  })
+    .then(response => response.json())
+    .then(({ html_url }) => {
+      sendMessage(bot, chatId, html_url, true, msgContext.message_id);
+    }).catch(() => { });
+};
+
+const createGist = (bot, msgContext, redisClient, text = '', checkingForCode = true) => {
   commandUtility.verifyCommand(redisClient, GIST_COMMAND, msgContext.from.id)
     .then((canExecuteCommand) => {
       if (canExecuteCommand) {
-        if (!Object.prototype.hasOwnProperty.call(msgContext, 'entities')) {
-          return;
-        }
-
-        if (msgContext.entities[0].type !== 'pre') {
-          return;
-        }
-
-        if (msgContext.text.length >= 200) {
-          return;
-        }
-
-        const chatId = msgContext.chat.id;
-        const { firstName = '', lastName = '', username = '' } = msgContext.from;
-        const fullName = firstName === '' && lastName === '' ? '' : `${firstName} ${lastName} `;
-        const user = username === '' ? '' : `(@${username})`;
-        const filename = `${new Date().toISOString()}.js`;
-        const gist = msgContext.text;
-
-        const body = {
-          description: gistCreated
-            .replace('#{fullName}', fullName)
-            .replace('#{user}', user)
-            .replace('#{telegramLink}', telegramLink)
-            .replace('#{githubLink}', githubLink),
-          public: true,
-          files: {
-            [filename]: {
-              content: gist
-            }
+        if (text === '' && msgContext.text.length >= MAX_LENGTH_GIST_TEXT) {
+          if (checkingForCode) {
+            sendMessage(bot, msgContext.chat.id, gistRecommendation, true, msgContext.message_id);
           }
-        };
-
-        fetch('https://api.github.com/gists', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-          },
-          body: JSON.stringify(body)
-        })
-          .then(response => response.json())
-          .then(({ html_url }) => {
-            sendMessage(bot, chatId, html_url, true, msgContext.message_id);
-          }).catch(() => { });
+        } else {
+          prepareAndSendGist(bot, msgContext, checkingForCode, text);
+        }
       }
     })
     .catch(() => { });
+};
+
+const checkForCode = (bot, msgContext, redisClient) => {
+  if (!Object.prototype.hasOwnProperty.call(msgContext, 'entities')) {
+    return;
+  }
+
+  if (msgContext.entities[0].type !== 'pre') {
+    return;
+  }
+
+  createGist(bot, msgContext, redisClient);
 };
 
 const sendOpenVeGithubLink = (bot, msgContext, command, redisClient) => {
@@ -137,5 +149,6 @@ module.exports = {
   checkAndSendRelease,
   checkForCode,
   sendOpenVeGithubLink,
-  sendCommunityRepo
+  sendCommunityRepo,
+  createGist
 };
