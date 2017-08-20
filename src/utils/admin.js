d9b8fac224e73e2853e6634bc79f5da49efe3d27
Leonardo Cabeza <leocabeza@users.noreply.github.com>
2017-08-20 17:15:39 -0500

    (Arregla) Tipo de grupo (#127)
    (Actualiza) Changelog

--- src/utils/admin.js
+++ src/utils/admin.js
@@ -51,11 +51,11 @@ const verifyAndSendUrl = (bot, msg, redisClient) => {
  * @param {boolean} privateChat
  */
 const verifyGroup = (msg, cb, mainGroup = true, adminGroup = false, privateChat = false) => {
-  if (mainGroup && msg.chat.type === 'group' && msg.chat.id.toString() === groupId) {
+  if (mainGroup && ['group', 'supergroup'].includes(msg.chat.type) && msg.chat.id.toString() === groupId) {
     return cb();
   }
 
-  if (adminGroup && msg.chat.type === 'group' && msg.chat.id.toString() === adminGroupId) {
+  if (adminGroup && ['group', 'supergroup'].includes(msg.chat.type) && msg.chat.id.toString() === adminGroupId) {
     return cb();
   }
 
