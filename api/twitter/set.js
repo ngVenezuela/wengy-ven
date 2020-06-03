d13e3b3e4722b54867921d9ca9c4bd2af47bd334
Leonardo Cabeza <leocabeza@users.noreply.github.com>
2020-06-02 21:02:12 -0500

    (Arregla) discard timeout, log url (#242)

--- api/twitter/set.js
+++ api/twitter/set.js
@@ -16,7 +16,6 @@ Sentry.init({ dsn: SENTRY_DSN });
 
 /* used to activate webhook whenever we change the APP_URL OR env variables have been compromised */
 export default async(request, response) => {
-  console.time('ms-set-test');
   try {
     if (request.query.consumerKey === TWITTER_CONSUMER_KEY) {
       const webhook = new Autohook({
@@ -46,7 +45,5 @@ export default async(request, response) => {
     }
 
     response.status(400).send('not ok');
-  } finally {
-    console.timeEnd('ms-set-test');
   }
 };
